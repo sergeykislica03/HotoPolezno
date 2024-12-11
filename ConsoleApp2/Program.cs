795eb1ec95d8e5eac563ab678ee5cd3faf712c62
sergeykislica03 <sergeykislica0.3@gmail.com>
2024-12-11 23:50:34 +0300

    DZ

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿int balans = 100;  
+Console.WriteLine("Добро пожаловать в банкомат!\n");
+Console.WriteLine("Ваш баланс:");
+Console.WriteLine(balans);
+Console.WriteLine("Введите сумму снятия:");
+int D = Convert.ToInt32(Console.ReadLine());
+int s = balans - D;
+Console.WriteLine("Ваш баланс:");
+Console.WriteLine(s);
